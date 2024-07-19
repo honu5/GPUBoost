64052b80883774f2f13b6b402bdbad7605d73d20
honu5 <liranso392@gmail.com>
2024-07-19 23:18:13 +0300

    feat: new demo added

--- demo/Diffusion/demo_controlnet.py
+++ demo/Diffusion/demo_controlnet.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
+# limitations under the License.
 # Configure dependencies before any external imports
 from demo_diffusion import deps
 deps.configure("sd")
